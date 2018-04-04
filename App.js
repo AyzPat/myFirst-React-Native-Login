5bd4bdf5bfb2d887d08cc3b10f05a83ae9322944
Ayaz <ayzpat@gmail.com>
2018-04-04 16:27:28 +0530

    init commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Application from './src/App.js';
+import allReducers from './src/reducers/index.js';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import { TextInput, TouchableOpacity } from 'react-native';
+const store = createStore(allReducers);
+export default class App extends React.Component {
+  render() {
+    return (
+      <Application />
+    );
+  }
+}
